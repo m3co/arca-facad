0ac1c6876519afad8c5f7e83be62ee1854e34283
Anton Novikau <anton.novikov.y@gmail.com>
2020-06-06 23:50:17 +0300

    Add Tabs components

---
+++ src/Tabs/Tabs.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import { Tabs as TabsMain, Tab as TabTitle } from '@material-ui/core';
+import Tab from './Tab';
+
+interface TabsProps {
+  tabs: {
+    label: string | number | React.ReactNode,
+    value: string | number | React.ReactNode,
+  }[]
+}
+
+const useStyles = makeStyles({
+  root: {
+    color: 'initial',
+    backgroundColor: '#fff',
+  },
+  indicator: {
+    backgroundColor: '#61dafb',
+  },
+});
+
+const useStylesTab = makeStyles({
+  root: {
+    flex: 1,
+    maxWidth: 'none',
+  },
+});
+
+const useStylesBar = makeStyles({
+  root: {
+    marginTop: 12,
+  },
+});
+
+const Tabs: React.FunctionComponent<TabsProps> = ({
+  tabs,
+}) => {
+  const classes = useStyles();
+  const classesTab = useStylesTab();
+  const classesBar = useStylesBar();
+
+  const [currentTab, setTab] = useState(0);
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setTab(newValue);
+  };
+
+  return (
+    <div>
+      <AppBar classes={classesBar} position='static'>
+        <TabsMain classes={classes} value={currentTab} onChange={handleChange} centered>
+          {
+            tabs.map((tab, index) => (
+              <TabTitle classes={classesTab} label={tab.label} id={`simple-tab-${index}`} key={`${tab.label}-${index}`}/>
+            ))
+          }
+        </TabsMain>
+      </AppBar>
+      {
+        tabs.map((tab, index) => (
+          <Tab value={currentTab} index={index} children={tab.value} key={String(index)}/>
+        ))
+      }
+    </div>
+  );
+};
+
+export default Tabs;
