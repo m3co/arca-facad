bdc1fb7c3d441ee567ce5f6cdd8e480659b70060
Anton Novikau <anton.novikau@icloud.com>
2020-06-15 23:16:29 +0300

    Add util for check new row for request

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,9 @@
-import { State } from 'arca-redux-v4';
+import get from 'lodash/get';
+import isObject from 'lodash/isObject';
+import { State, Row } from 'arca-redux-v4';
 import { FACAD_CFT_AAU_COLUMNS, FACAD_PRE_CFT_AAU_KEY_COLUMNS } from './constants/columns';
 import { FACAD_CFT_AAU, FACAD_PRE_CFT_AAU_KEY } from './constants/sources';
+import * as CELLS from './constants/cells';
 
 export const getColumnOrder = (source: keyof State['Source']) => {
   switch (source) {
@@ -11,4 +14,32 @@ export const getColumnOrder = (source: keyof State['Source']) => {
     default:
       return [];
   }
+};
+
+export const checkAllCells = (row: Row, namesCells: string[]) => {
+  const result = namesCells.reduce((acc: Row, cur: keyof Row) => {
+    const value = row[cur];
+
+    if (isObject(value)) {
+      switch (cur) {
+        case CELLS.PROJECT:
+          acc[cur] = get(value, 'PK.ID', '' as never);
+          break;
+        case CELLS.CONSTRAINT_FIELD:
+        case CELLS.KEYNOTE_FIELD:
+        case CELLS.QUANTITY_FIELD:
+          acc[cur] = get(value, 'PK.Field', '' as never);
+          break;
+        default:
+          acc[cur] = get(value, `PK.${cur}`, '' as never);
+          break;
+      }
+    } else {
+      acc[cur] = value;
+    }
+
+    return acc;
+  }, {} as Row);
+
+  return result;
 };
