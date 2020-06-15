773352e56c731607c6cc089345031cae9a3521ac
Anton Novikau <anton.novikau@icloud.com>
2020-06-15 23:13:34 +0300

    Add constants

---
+++ src/utils/constants/cells.ts
@@ -0,0 +1,6 @@
+export const ID = 'ID';
+export const FIELD = 'Field';
+export const PROJECT = 'Project';
+export const KEYNOTE_FIELD  = 'KeynoteField';
+export const CONSTRAINT_FIELD = 'ConstraintField';
+export const QUANTITY_FIELD = 'QuantityField';
---
+++ src/utils/constants/selects.ts
@@ -0,0 +1,8 @@
+export const REPORT_TYPE = [
+  'Schedule',
+  'MaterialTakeoff',
+  'KeynoteLegend',
+  'KeySchedule',
+  'RevisionSchedule',
+  'NoteBlock',
+];
